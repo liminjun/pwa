ef093dce8e6e9769055fe1679ad745bf82710f23
liminjun <v-leeli@hotmail.com>
2017-12-05 09:17:28 +0800

    feat:cache all static files.

--- 2-application-mainfest/public/src/js/app.js
+++ 2-application-mainfest/public/src/js/app.js
@@ -15,61 +15,6 @@ window.addEventListener('beforeinstallprompt',function(event){
     return false;
 });
 
-var promise=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        //resolve("This is executed once the timer is done!");
-        reject({code:500,message:"An error occurred!"});
-    }, 3000);
-});
-//fetch get
-fetch('http://httpbin.org/ip')
-.then(function(response){
-    console.log(response);
-    return response.json();
-}).then(function(data){
-    console.log(data);
-})
-.catch(function(err){
-    console.log(err);
-});
-
-//fetch post
-fetch('http://httpbin.org/post',{
-    method:"POST",
-    headers:{
-        'Content-Type':"application/json",
-        'Accept':"application/json"
-    },
-    mode:"cors",
-    body:JSON.stringify({
-        message:"Does this work?"
-    })
-})
-.then(function(response){
-    console.log(response);
-    return response.json();
-}).then(function(data){
-    console.log(data);
-})
-.catch(function(err){
-    console.log(err);
-});
-
-// promise.then(function(text){
-//     return text;
-// },function(error){
-//     console.log(error.code,error.message);
-// }).then(function(newText){
-//     console.log(newText);
-// });
-
-promise.then(function(text){
-    return text;
-}).then(function(newText){
-    console.log(newText);
-}).catch(function(error){
-    console.log(error.code,error.message);
-});
 
 
 console.log("this is executed right after setTimeout()");
--- 2-application-mainfest/public/sw.js
+++ 2-application-mainfest/public/sw.js
@@ -4,9 +4,26 @@ self.addEventListener('install',function(event){
         caches.open("static")
         .then(function(cache){
             console.log("[Service Workder] Precaching App Sheel.");
-            cache.add("/");
+
             cache.add("/index.html");
-            cache.add("/src/js/app.js");
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/js/promise.js',
+                '/src/js/fetch.js',
+                '/src/js/material.min.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/css/help.css',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+                '/favicon.ico'
+            ]);
+
         })
     );
 });
